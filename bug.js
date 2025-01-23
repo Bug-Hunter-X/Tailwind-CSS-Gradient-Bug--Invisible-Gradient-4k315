dd64c737aa20c91fec8221b1ae86e53866416a49
BugHunter-X <askzenai@gmail.com>
2025-01-23 11:34:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,5 @@
+```javascript
+<div class="bg-gradient-to-r from-blue-500 to-purple-500">
+  </div>
+```
+This code snippet uses Tailwind CSS's gradient feature. However, if the `from` and `to` colors are too similar, the gradient might not be visible, resulting in a subtle bug where the background appears solid instead of a gradient.
