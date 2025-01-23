5cb06f840a1ce489199e59ab3fe9a2b6375c17f6
BugHunter-X <askzenai@gmail.com>
2025-01-23 11:34:05 +0100

    Add solution.js file

---
+++ solution.js
@@ -0,0 +1,10 @@
+```javascript
+<div class="bg-gradient-to-r from-blue-500 to-purple-500">
+  </div>
+```
+This is better because the colors are distinctive.
+```javascript
+<div class="bg-gradient-to-r from-blue-500 to-blue-700">
+  </div>
+```
+This would cause a bug, because the gradient is too subtle.
